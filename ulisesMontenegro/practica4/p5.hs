29bcb4b4c7e86bddbe42a0f02430d1913a71010c
ulimonte2005 <68628188+ulimonte05@users.noreply.github.com>
2024-04-22 15:28:34 -0300

    h

--- ulisesMontenegro/practica4/p5.hs
+++ ulisesMontenegro/practica4/p5.hs
@@ -61,20 +61,6 @@ quitarTodos :: (Eq t) => t -> [t] -> [t]
 quitarTodos a [] = []
 quitarTodos a (x:xs) = if (x == a) then (if (pertenece a xs) then quitarTodos a xs else xs) else if (pertenece a xs) then quitarTodos a (xs++[x]) else [x]++xs
 
-<<<<<<< Updated upstream
--- g 
-
--- eliminarRepetidos :: (Eq t) => [t] -> [t] 
--- eliminarRepetidos [] = []
--- eliminarRepetidos [x] = [x]
--- eliminarRepetidos (x:xs) = if (pertenece x xs) then eliminarRepetidos xs else xs
-
--- h
-
--- falta este xd
-
--- i
-=======
 -- g (se usa todosDistintos y pertenece)
 
 eliminarRepetidos :: (Eq t) => [t] -> [t] 
@@ -92,7 +78,112 @@ mismosElementos [] [] = True
 mismosElementos xs ys = listasMismosElementos (eliminarRepetidos xs) (eliminarRepetidos ys)
 
 -- i (se usa reverso)
->>>>>>> Stashed changes
 
 capicua :: (Eq t) => [t] -> Bool
 capicua xs = if (xs == (reverso xs)) then True else False
+
+-- 3er ejerciciooooooo
+
+-- a
+
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+-- b
+
+productoria :: [Integer] -> Integer
+productoria [] = 1
+productoria (x:xs) = x * productoria xs
+
+-- c
+
+maximo :: [Integer] -> Integer
+maximo [x] = x
+maximo (x:xs) = if (x > xs!!0) then maximo (xs++[x]) else maximo xs
+
+minimo :: [Integer] -> Integer
+minimo [x] = x
+minimo (x:xs) = if (x < xs!!0) then minimo (xs++[x]) else minimo xs
+
+-- d 
+
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN a [] = []
+sumarN a (x:xs) = x + a : sumarN a xs 
+
+-- e
+
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero [] = []
+sumarElPrimero (x:xs) = sumarN x ([x]++xs)
+
+-- f
+
+sumarUltimo :: [Integer] -> [Integer]
+sumarUltimo [] = []
+sumarUltimo xs = sumarN (ultimo xs) xs
+
+-- g
+
+algunImpar :: [Integer] -> Bool
+algunImpar [] = False
+algunImpar (x:xs) = if (mod x 2 /= 0) then True else algunImpar (xs)
+
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares (x:xs) = if (algunImpar ([x]++xs)) then (if (mod x 2 /= 0) then pares xs else pares (xs++[x])) else ([x]++xs)
+
+-- h
+
+algunNoMultiplo :: Integer -> [Integer] -> Bool
+algunNoMultiplo a [] = False
+algunNoMultiplo a (x:xs) = if (mod x a /= 0) then True else algunNoMultiplo a (xs)
+
+multiplos :: Integer -> [Integer] -> [Integer]
+multiplos a [] = []
+multiplos a (x:xs) = if (algunNoMultiplo a ([x]++xs)) then (if (mod x a /= 0) then multiplos a xs else multiplos a (xs++[x])) else ([x]++xs)
+
+-- i
+
+ordenarInverso :: [Integer] -> [Integer]
+ordenarInverso [] = []
+ordenarInverso xs = max : ordenarInverso (quitar max xs)  
+  where max = maximo xs      
+
+ordenar :: [Integer] -> [Integer]
+ordenar [] = []
+ordenar xs = min : ordenar (quitar min xs)  
+  where min = minimo xs      
+
+-- 4to ejerciciooooooooaooaooaoaoa
+
+-- a
+
+sacarBlancosRepetidos :: [Char] -> [Char]
+sacarBlancosRepetidos [] = []
+sacarBlancosRepetidos [x] = [x]
+sacarBlancosRepetidos (x:y:xs)
+    | x == ' ' && y == ' ' = sacarBlancosRepetidos (x:xs)
+    | otherwise = x : sacarBlancosRepetidos (y:xs)
+
+-- b
+
+contarPalabras :: [Char] -> Integer
+contarPalabras [] = 0
+contarPalabras [x] = 1
+contarPalabras (x:xs)
+    | x == ' ' = 1 + contarPalabras xs
+    | otherwise = contarPalabras (xs)
+
+-- c
+
+palabras :: [Char] -> [[Char]]
+palabras [] = []
+palabras [x] = [x]
+palabras (x:y:xs)
+    | y == ' ' = x ++ palabras xs
+    | otherwise = palabras (xs)
+
+-- d
+
